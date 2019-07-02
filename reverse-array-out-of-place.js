5e6aafd36f137ed4b5de1053b62dfaaad9d3ac97
George McCadden III <thethirdone.g@gmail.com>
2019-07-02 05:31:28 -0700

    I had some unsaved pushed code.

---
+++ reverse-array-out-of-place.js
@@ -0,0 +1,22 @@
+// Reversing an Array (Out of Place)
+
+// Create a function, reverseArray, which takes an array as an argument and produces a new array that has the same elements in the inverse order.
+
+let firstArr = ['Raffy', 'Miguel', 'Lori', 'Noy', 'Nana'];
+
+let secondArr = ['Camille', 'George III', 'Becky', 'George Jr'];
+
+// This newlyMadeArr will be filled with strings which will represent the array passed in but in reverse.
+
+// ** let newlyMadeArr = []; **
+
+// 1) I need to add the last element of the passed in array to the beginning of newlyMadeArr - loop through the array until the end to find last element to add.
+
+// 2) Print the contents of newlyMadeArr - After the loop has finished, print out the reversed array to the screen.
+
+const function reverseArray(arr) {
+  let newlyMadeArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newlyMadeArr.push()
+  }
+}
