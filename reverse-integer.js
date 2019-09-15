b0dce19ecd7914c7417a0bb0c0b346674a4debe9
George McCadden III <thethirdone.g@gmail.com>
2019-09-15 14:42:14 -0700

    splitting the string into an array

--- reverse-integer.js
+++ reverse-integer.js
@@ -6,6 +6,12 @@ const reversedNum = num => parseFloat(num.toString().split('').reverse().join(''
 
 // Algorithm :
 
-// Convert the number to a string - "num.toString()" converts the given number to a string. Doing this will allow me to use "split()" function.
+// Convert the number to a string - "num.toString()" - converts the given number to a string. Doing this will allow me to use "split()" function.
+// ex. let num = -5432100;
+//     num.toString(); // num equals '-5432100'
 
-//
+
+// Split the string into an array - "num.split('')" - converts the string into an array of characters. Doing this will allow me to use the array reverse functions, which does not work on a string.
+// ex. num = '-5432100'
+//     num.split(''); // num equals ['-', '5', '4', '3', '2', '1', '0', '0'];
+
