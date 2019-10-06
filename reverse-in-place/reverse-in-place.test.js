8b9c33c7a3d976e62001983c4202f22b0fa0677a
George McCadden III <thethirdone.g@gmail.com>
2019-10-05 19:08:25 -0700

    Adding test number 2

--- reverse-in-place/reverse-in-place.test.js
+++ reverse-in-place/reverse-in-place.test.js
@@ -5,3 +5,7 @@ const reverse = require('./reverse-in-place');
 test('Reverse function exists', () => {
     expect(reverse).toBeDefined();
 });
+
+test('Reverse function reverses a string', () => {
+    expect(reverse('abcd')).toEqual('dcba');
+});
