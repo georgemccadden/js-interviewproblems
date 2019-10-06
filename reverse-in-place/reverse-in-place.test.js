c07f0397fe5feeb08a1ce1fc8a49d1932ca6f4bc
George McCadden III <thethirdone.g@gmail.com>
2019-10-05 19:06:39 -0700

    Adding test number 1

---
+++ reverse-in-place/reverse-in-place.test.js
@@ -0,0 +1,7 @@
+// Learned these tests scripts from Stephen. This will help with practicing the 'Reverse String' interview problem.
+
+const reverse = require('./reverse-in-place');
+
+test('Reverse function exists', () => {
+    expect(reverse).toBeDefined();
+});
