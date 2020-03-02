3c85e5ab4aec32a55fa53076befb31a8d55707ee
George McCadden III <thethirdone.g@gmail.com>
2020-03-01 16:18:17 -0800

    Just writing out the problem domain and conditions

---
+++ palindromes/index.js
@@ -0,0 +1,4 @@
+// Problem Domain : 
+
+// Given a string, return true if the string is a palindrome or false if it is not. Palindromes are strings that form the same word if they are reversed - Do include spaces and punctuation when determining if the string is a palindrome.
+
