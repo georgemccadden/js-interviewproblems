1cc4fc4e69b614d0ce9ddd05145981cc9f00265a
George McCadden III <thethirdone.g@gmail.com>
2020-03-01 16:22:49 -0800

    This is a really straightforward solution

--- palindromes/index.js
+++ palindromes/index.js
@@ -2,3 +2,21 @@
 
 // Given a string, return true if the string is a palindrome or false if it is not. Palindromes are strings that form the same word if they are reversed - Do include spaces and punctuation when determining if the string is a palindrome.
 
+// Visuals :
+
+// palindrome("abba") === true
+// palindrome("abcdefg") === false
+
+// Algorithm :
+
+
+
+
+
+// Code :
+
+function palindrome(str) {
+    const reversed = str.split('').reverse().join('');
+
+    return str === reversed;
+}
