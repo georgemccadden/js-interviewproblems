d9e5c04468f14860bb34a00f68a2638bafdd7034
George McCadden III <thethirdone.g@gmail.com>
2020-03-04 00:21:38 -0800

    Setting up another reverse problem

---
+++ reverseint/index.js
@@ -0,0 +1,12 @@
+// Problem Domain :
+
+// Given an integer, return an integer that is the reverse ordering of numbers.
+
+// Visual :
+
+// reverseInt(15) === 51
+// reverseInt(981) === 189
+// reverseInt(500) === 5
+// reverseInt(-15) === -51
+// reverseInt(-90) === -9
+
