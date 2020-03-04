e954e1ebf548d6e4b863972b4fa63adc3bdbdae4
George McCadden III <thethirdone.g@gmail.com>
2020-03-04 00:25:26 -0800

    Making progress

--- reverseint/index.js
+++ reverseint/index.js
@@ -2,6 +2,7 @@
 
 // Given an integer, return an integer that is the reverse ordering of numbers.
 
+
 // Visual :
 
 // reverseInt(15) === 51
@@ -10,3 +11,16 @@
 // reverseInt(-15) === -51
 // reverseInt(-90) === -9
 
+
+// Algorithm 1 :
+
+// - Turn the number into a string.
+// - Reverse the string.
+// - Determine if integer passed in was negative or not, if so make the inverse negative as well but if not make it positive.
+// - Return the integer.
+
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+
+    if ()
+}
