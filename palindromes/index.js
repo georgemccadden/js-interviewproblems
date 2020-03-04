9c8eac2bc643032c967fc68db284c4790efb908f
George McCadden III <thethirdone.g@gmail.com>
2020-03-03 23:09:32 -0800

    Adding another solution for the palindrome problem

--- palindromes/index.js
+++ palindromes/index.js
@@ -2,15 +2,18 @@
 
 // Given a string, return true if the string is a palindrome or false if it is not. Palindromes are strings that form the same word if they are reversed - Do include spaces and punctuation when determining if the string is a palindrome.
 
+
 // Visuals :
 
 // palindrome("abba") === true
 // palindrome("abcdefg") === false
 
+
 // Algorithm :
 
-
-
+// - Create a variable for the passed in string.
+// - Reverse the passed in string.
+// - Compare the passed in string with that reversed string to determine whether or not they are equal, if so then return true.
 
 
 // Solution 1 :
@@ -21,3 +24,4 @@ function palindrome(str) {
     return str === reversed;
 }
 
+// Solution 2 :
