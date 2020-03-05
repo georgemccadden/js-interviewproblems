664df9dbe71f444f6de8e9d17e4cda29facf6ccd
George McCadden III <thethirdone.g@gmail.com>
2020-03-04 23:19:38 -0800

    Some questions which allude to this technique

--- maxchar/index.js
+++ maxchar/index.js
@@ -2,8 +2,14 @@
 
 // Given a string, return the character that is most commonly used in the string.
 
+// ** Common Questions **
+
+// What is the most common character in the string?
+// Does string A have the same characters as string B? (Is it an anagram?)
+// Does the given string have any repeated characters in it?
 
 // Visual :
 
 // maxChar("abcccccccd") = "c"
 // maxChar("apple 1231111") = "1"
+
