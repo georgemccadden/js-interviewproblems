2f20104eed7e9b3fae23ede3c25d2f251568d74c
George McCadden III <thethirdone.g@gmail.com>
2020-03-05 15:57:25 -0800

    Finished my proposed algorithm

--- maxchar/index.js
+++ maxchar/index.js
@@ -21,5 +21,6 @@
 // Create a variable to hold the letter that's associated with that max number.
 // From that passed in string, build a character map representing the string and the amount of characters.
 // If a character isn't yet present in the map, start it off at 1 but if that character is present add one to its existing amount.
-// 
+// Iterate through the character map created from the passed in string and update the max number and its associated character.
+// After the iteration is complete, return the character with the max number.
 
