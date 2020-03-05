9975aaf5011a73a95d3aa1b74c946edf89092321
George McCadden III <thethirdone.g@gmail.com>
2020-03-04 23:06:52 -0800

    Oh, I forgot to comment the smelly code

--- reverseint/index.js
+++ reverseint/index.js
@@ -21,16 +21,16 @@
 
 // Solution 1 :
 
-function reverseInt(n) {
-    const reversed = n.toString().split('').reverse().join('');
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('');
 
-    if (n < 0) {
-        return parseInt(reversed) * -1;
-    }
-    return parseInt(reversed);
-}
+//     if (n < 0) {
+//         return parseInt(reversed) * -1;
+//     }
+//     return parseInt(reversed);
+// }
 
-module.exports = reverseInt;
+// module.exports = reverseInt;
 
 // Solution 1 (cleaner) :
 
