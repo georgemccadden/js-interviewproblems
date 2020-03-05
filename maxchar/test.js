6e1b6a533b9d5c508e1448866d2a8992240d6c91
George McCadden III <thethirdone.g@gmail.com>
2020-03-04 23:12:57 -0800

    This is a very common interview problem

---
+++ maxchar/test.js
@@ -0,0 +1,10 @@
+// Problem Domain :
+
+// Given a string, return the character that is most commonly used in the string.
+
+
+// Visual :
+
+// maxChar("abcccccccd") = "c"
+// maxChar("apple 1231111") = "1"
+
