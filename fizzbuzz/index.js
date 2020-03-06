88a28779c6f42bbd1b65f1d2863881963f7ab57f
George McCadden III <thethirdone.g@gmail.com>
2020-03-05 17:22:13 -0800

    Writing out the problem domain and providing a visual

---
+++ fizzbuzz/index.js
@@ -0,0 +1,18 @@
+// Problem Domain :
+
+// Write a program which console logs the numbers from 1 to n. For multiples of three print "fizz" instead of the number and for multiples of five print "buzz". For numbers which are multiples of both print "fizzbuzz".
+
+
+// Visual :
+
+// fizzBuzz(5);
+// 1
+// 2
+// fizz
+// 4
+// buzz
+// ...
+// 14
+// fizzbuzz
+// 16
+
