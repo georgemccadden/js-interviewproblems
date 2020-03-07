f1f723fa0c29cf80e07b8f289b68232ad2851413
George McCadden III <thethirdone.g@gmail.com>
2020-03-06 22:24:14 -0800

    Wrote out an algorithm for this beast of a problem

--- chunk/index.js
+++ chunk/index.js
@@ -8,3 +8,15 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+// Algorithm 1:
+
+// - Create a function which accepts an array and number
+// - Create an empty array to hold "chunked" data
+
+// - For each element in the original array :
+// - * Iterate through the array passed into the function *
+// - * Retrieve the last element in the "chunked" array *
+// - * If the last element doesn't exist or if its length is equal to chunk size then push a new chunk into "chunked" array with the current element.
+// - * If anything else, add the current element to the "chunked" array.
