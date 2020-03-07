bb5d6f8ed25e4f14922caaa625f2972023da20ab
George McCadden III <thethirdone.g@gmail.com>
2020-03-06 22:07:41 -0800

    Practicing one that I have never seen before

---
+++ chunk/index.js
@@ -0,0 +1,10 @@
+// Problem Domain : Given an array and chuck size, divide the array into many subarrays where each subarray is of length size.
+
+
+// Visual :
+
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
