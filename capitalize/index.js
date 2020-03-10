78d0fdba72b67b25e98c6291169173636395a6c3
George McCadden III <thethirdone.g@gmail.com>
2020-03-09 18:51:26 -0700

    Writing out a clear problem domain

---
+++ capitalize/index.js
@@ -0,0 +1,3 @@
+// Problem Domain :
+
+// Write a function which accepts a string. This function should capitalize the first letter of each word in the string and then return the capitalized string.
