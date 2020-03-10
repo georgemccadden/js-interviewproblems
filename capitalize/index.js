92542a91c067fea0077e5aaae4c3ee0e50bbefad
George McCadden III <thethirdone.g@gmail.com>
2020-03-10 16:04:47 -0700

    I think that this makes more sense

--- capitalize/index.js
+++ capitalize/index.js
@@ -14,7 +14,9 @@
 
 // - Make an empty array for the words.
 // - Split the input string by spaces in order to create an array.
-// - 
+// - For each word in the array, capitalize the first letter, join the first letter with the rest of the string, and then push the results into the array for the words.
+// - Join the array for the words into a string and return it.
+
 
 // Solution 1 :
 
