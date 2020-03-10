cbd864cd145b9a385a99a65c70eb0730d14c1e33
George McCadden III <thethirdone.g@gmail.com>
2020-03-09 18:54:11 -0700

    Had to provide some visual examples

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,3 +1,12 @@
 // Problem Domain :
 
 // Write a function which accepts a string. This function should capitalize the first letter of each word in the string and then return the capitalized string.
+
+
+// Visual :
+
+// capitalize('a short sentence') --> 'A Short Sentence'
+// capitalize('a lazy fox') --> 'A Lazy Fox'
+// capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+
