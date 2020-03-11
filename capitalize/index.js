55c680a311ef85912f1975a1f44af28b2ac6e105
George McCadden III <thethirdone.g@gmail.com>
2020-03-10 23:34:59 -0700

    I messed something up

--- capitalize/index.js
+++ capitalize/index.js
@@ -39,18 +39,5 @@
 
 // Solution 2 :
 
-function capitalize(str) {
-    let result = str[0].toUpperCase();
-
-    for (let i = 1; i < str.length; i++) {
-        if (words[i - 1] === ' ') {
-            result += str[i].toUpperCase();
-        } else {
-            result += str[i];
-        }
-
-        return result;
-    }
-}
 
 module.exports = capitalize;
