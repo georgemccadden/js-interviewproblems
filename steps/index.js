a9e4af322c0ea51be90a7a290190bdc90b2a40e0
George McCadden III <thethirdone.g@gmail.com>
2020-03-11 17:49:43 -0700

    Wrote out a reasonable problem domain

---
+++ steps/index.js
@@ -0,0 +1,3 @@
+// Problem Domain :
+
+// Write a function which accepts a positive number 'n'. The function should console log a step shape with 'n' levels using the # character. Make sure the step has spaces on the right hand side.
