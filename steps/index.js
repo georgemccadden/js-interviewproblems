ad5498f6b2638c9a1b7adad59ec741197d5963d4
George McCadden III <thethirdone.g@gmail.com>
2020-03-11 17:53:40 -0700

    It is not easy creating visuals of this

--- steps/index.js
+++ steps/index.js
@@ -1,3 +1,21 @@
 // Problem Domain :
 
 // Write a function which accepts a positive number 'n'. The function should console log a step shape with 'n' levels using the # character. Make sure the step has spaces on the right hand side.
+
+
+// Visual :
+
+// steps(2)
+// '# '
+// '##'
+
+// steps(3)
+// '#  '
+// '## '
+// '###'
+
+// steps(4)
+// '#   '
+// '##  '
+// '### '
+// '####'
