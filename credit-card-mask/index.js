1d7ad82e60e66dfd424f3c8350ab7d8e81e80684
George McCadden III <thethirdone.g@gmail.com>
2020-03-22 21:12:14 -0700

    Adding a challenge from Codewars

---
+++ credit-card-mask/index.js
@@ -0,0 +1,6 @@
+// Problem Domain : 
+
+// You're buying something and you're asked whether your credit card number, phone number, or the answer to your secret question is still correct. However, to avoid someone looking over your shoulder, the password is not fully shown to the screen. Mask it!
+
+
+// Visual :
