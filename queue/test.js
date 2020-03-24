e89ad4b3490646b1a79b780a2f31f3531971e56e
George McCadden III <thethirdone.g@gmail.com>
2020-03-23 23:40:03 -0700

    Can we add elements

--- queue/test.js
+++ queue/test.js
@@ -3,3 +3,10 @@ const Queue = require('./index');
 test('Queue is a class', () => {
   expect(typeof Queue.prototype.constructor).toEqual('function');
 });
+
+test('can add elements to a queue', () => {
+    const q = new Queue();
+    expect(() => {
+      q.add(1);
+    }).not.toThrow();
+  });
