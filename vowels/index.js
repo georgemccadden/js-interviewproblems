edbfe115114ebfde4e69215695362f179073ae66
George McCadden III <thethirdone.g@gmail.com>
2020-03-23 23:35:12 -0700

    Setting myself up for a win

---
+++ vowels/index.js
@@ -0,0 +1,16 @@
+// Problem Domain :
+
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+
+
+// Visual :
+
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {}
+
+module.exports = vowels;
