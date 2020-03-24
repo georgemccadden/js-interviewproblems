419d8da2480eb5538f8f29b79c94a1c023782711
George McCadden III <thethirdone.g@gmail.com>
2020-03-23 23:38:03 -0700

    Adding a new challenge

---
+++ queue/index.js
@@ -0,0 +1,15 @@
+// Problem Domain :
+
+// Create a queue data structure.  The queue should be a class with methods 'add' and 'remove'. Adding to the queue should store an element until it is removed.
+
+
+// Visual :
+
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+
+class Queue {}
+
+module.exports = Queue;
