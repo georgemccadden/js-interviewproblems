ab6fadeade4f3db514a6ad9fdb2d2d970b39aea6
George McCadden III <thethirdone.g@gmail.com>
2020-03-30 23:54:20 -0700

    Added a few more

--- pyramids/test.js
+++ pyramids/test.js
@@ -1,5 +1,14 @@
 const pyramid = require('./index');
 
 beforeEach(() => {
-  jest.spyOn(console, 'log');
+    jest.spyOn(console, 'log');
 });
+
+afterEach(() => {
+    console.log.mockRestore();
+  });
+  
+  test('pyramid is a function', () => {
+    expect(typeof pyramid).toEqual('function');
+  });
+  
