3964af949f5f55c603ee8698168d6ae3bc5502a5
George McCadden III <thethirdone.g@gmail.com>
2020-04-01 23:10:08 -0700

    Need a visual for an example

--- pyramids/index.js
+++ pyramids/index.js
@@ -1,3 +1,5 @@
 // Problem Domain :
 
 // Write a function that accepts a positive number N. The function should console log a pyramid shape with N levels using the '#' character. Make sure the pyramid has spaces on both the left *and* right hang sides.
+
+// Visual :
