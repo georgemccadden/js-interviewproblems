9c4183c517ddcf0c99cf4d19ceb8c197b01598aa
George McCadden III <thethirdone.g@gmail.com>
2020-04-02 23:43:41 -0700

    They would not pass because they were not exported

--- pyramid/index.js
+++ pyramid/index.js
@@ -26,7 +26,7 @@
 
 function pyramid(n) {
     const midpoint = Math.floor((2 * n - 1) / 2);
-    
+
     for (let row = 0; row < n; row++) {
         let level = '';
 
@@ -40,3 +40,5 @@ function pyramid(n) {
         console.log(level);
     }
 }
+
+module.exports = pyramid;
