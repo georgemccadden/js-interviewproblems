f6f4c81034eddc6a5b654e209fffa45707578f04
George McCadden III <thethirdone.g@gmail.com>
2020-04-02 23:34:15 -0700

    Need to create a midpoint variable

--- pyramids/index.js
+++ pyramids/index.js
@@ -25,9 +25,12 @@
 // Code :
 
 function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
     for (let row = 0; row < n; row++) {
         let level = '';
 
-        
+        for (let column = 0; column < 2 * n - 1; column++) {
+            
+        }
     }
 }
