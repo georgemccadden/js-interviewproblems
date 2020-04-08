8c96c03c5085c644de7d2c344994cd477e7a36d3
George McCadden III <thethirdone.g@gmail.com>
2020-04-08 00:15:46 -0700

    I think I did this correctly

--- pyramid/index.js
+++ pyramid/index.js
@@ -62,5 +62,13 @@ function recursivePyramid(n, row = 0, level = '') {
         console.log(level);
         return recursivePyramid(n, row + 1);
     }
+
+    const midpoint = Math.floor = Math.floor((2 * n - 1) / 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
 }
 module.exports = pyramid;
