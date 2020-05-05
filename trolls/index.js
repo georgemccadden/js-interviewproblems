f886c1bc61538812eec73e1783ca8cdf070b9936
George McCadden III <thethirdone.g@gmail.com>
2020-05-04 22:53:53 -0700

    Problem domain with an example

---
+++ trolls/index.js
@@ -0,0 +1,11 @@
+// Problem Domain :
+
+// Trolls are attacking your comment section! A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat. Write a function that takes a string and return a new string with all vowels removed.
+
+
+// Example :
+
+// The string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!" * 'y' isn't considered a vowel *
+
+
+// 
