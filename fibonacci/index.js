9f3faac10a3163480cdeea8272681c285ed952da
George McCadden III <thethirdone.g@gmail.com>
2020-05-12 23:41:29 -0700

    Stating the obvious

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -36,6 +36,10 @@ function fib(n) {
 
 // Recursive Solution :
 
-
+function fib(n) {
+    if (n < 2) {
+        return n; // Obviously the 0 index and first index will be 0 and 1
+    }
+}
 
 module.exports = fib;
