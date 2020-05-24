bdf24dd9dd272d161bf1c06006a8fb55238431ca
George McCadden III <thethirdone.g@gmail.com>
2020-05-24 00:07:34 -0700

    Presenting a visual example and explanation

--- stockprices/index.js
+++ stockprices/index.js
@@ -1,1 +1,8 @@
 // Problem Domain : Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
+
+// Example/Visual :
+
+// const stockPrices = [10, 7, 5, 8, 11, 9];
+
+// getMaxProfit(stockPrices);
+// * Should return 6 because buying for $5 and selling for $11 would be the most profitable.
