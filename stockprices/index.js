ff565d420aee9325bd32bd140382eb3627879a30
George McCadden III <thethirdone.g@gmail.com>
2020-05-24 00:08:13 -0700

    Creating some space and preparing to write my algorithm

--- stockprices/index.js
+++ stockprices/index.js
@@ -1,5 +1,6 @@
 // Problem Domain : Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
 
+
 // Example/Visual :
 
 // const stockPrices = [10, 7, 5, 8, 11, 9];
