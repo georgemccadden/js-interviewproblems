fbc6ae346e05e53c6db3c2448314de07ce0901eb
George McCadden III <thethirdone.g@gmail.com>
2020-05-23 23:33:23 -0700

    Just a little practice

---
+++ stockprices/index.js
@@ -0,0 +1,1 @@
+// Problem Domain : Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
