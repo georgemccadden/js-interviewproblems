4e488abf9628772d1733d3da9d48a31c52fa0539
George McCadden III <thethirdone.g@gmail.com>
2020-06-15 23:37:52 -0700

    Working on a calendar practice problem

---
+++ hical/index.js
@@ -0,0 +1,7 @@
+// Your company has built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available. To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as objects with integer properties startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
+
+// Example :
+
+// { startTime : 2, endTime : 3 } // meeting from 10:00 am - 10:30 am
+// { startTime : 6, endTime : 9 } // meeting from 12:00 pm - 1:30 pm
+
