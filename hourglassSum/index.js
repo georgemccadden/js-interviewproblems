e7acaff5c345c2b19462cf6c59a34cf18dc2fd97
George McCadden III <thethirdone.g@gmail.com>
2020-09-22 18:02:38 -0700

    Stating the problem domain

---
+++ hourglassSum/index.js
@@ -0,0 +1,12 @@
+// Problem Domain :
+
+// Given a 6 x 6 2D array 'arr', there are 16 hourglasses inside. An hourglass is a subset of values like :
+
+// a b c
+//   d
+// e f g
+
+// An hourglass' sum is the sum of its values. Calculate the hourglass sum of every hourglass in 'arr', then print the maximum hourglass sum.
+
+
+// Example :
