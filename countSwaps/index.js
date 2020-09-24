b93fd02ea65120bfab0bad16301b5cd3b99bf9d6
George McCadden III <thethirdone.g@gmail.com>
2020-09-23 22:47:44 -0700

    This satisfied all of the test cases

--- countSwaps/index.js
+++ countSwaps/index.js
@@ -1,3 +1,29 @@
 // Problem Domain :
 
-// Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+// Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm provided (Hacker Rank). Once sorted, print the following three lines:
+
+// CODE :
+
+function countSwaps(arr) {
+    let numSwaps = 0;
+    let thereWereNoSwaps = true;
+
+    for (let i = 0; i < arr.length - 1; i += 1) {
+        for (let k = 0; k < arr.length - 1; k += 1) {
+            if (arr[k] > arr[k + 1]) {
+                const temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+                numSwaps += 1;
+                thereWereNoSwaps = false;
+            }
+        }
+
+        if (thereWereNoSwaps) break;
+    }
+
+    console.log(`Array is sorted in ${numSwaps} swaps.`);
+    console.log(`First Element: ${a[0]}`);
+    console.log(`Last Element: ${a[a.length - 1]}`);
+
+}
