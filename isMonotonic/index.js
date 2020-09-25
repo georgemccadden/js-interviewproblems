59cbcec048ec6880aa623a426d0cf6158037814c
George McCadden III <thethirdone.g@gmail.com>
2020-09-25 16:35:15 -0700

    Code a solution which passes all tests

--- isMonotonic/index.js
+++ isMonotonic/index.js
@@ -10,3 +10,25 @@
 
 // Input - [1, 3, 2, 6, 8]
 // Output - false
+
+
+// Algorithm :
+
+// - Keep a tally of whether or not the next element is equal/decreasing or equal/increasing
+// - iterate through the entire array
+// - return whether or not the tallies are equal to the length of the array minus one.
+
+
+// Code :
+
+function isMonotonc(arr) {
+    let increasing = 0;
+    let decreasing = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= arr[i + 1]) increasing++;
+        if (arr[i] >= arr[i + 1]) decreasing++;
+    }
+    return (increasing === arr.length - 1 || decreasing === arr.length - 1);
+
+}
