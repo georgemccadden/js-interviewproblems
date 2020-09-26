a7cf7cfe4373c505583b336f406f517e3a4a8093
George McCadden III <thethirdone.g@gmail.com>
2020-09-26 12:56:43 -0700

    Provide an example

---
+++ thirdMax/index.js
@@ -0,0 +1,4 @@
+// Problem Domain :
+
+// Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
