63f7108ccfde0994c1f964abe41272fdc353c85b
George McCadden III <thethirdone.g@gmail.com>
2020-09-28 19:08:04 -0700

    Coded my algorithm and passed all tests

--- sortArrayByParity/index.js
+++ sortArrayByParity/index.js
@@ -18,3 +18,27 @@
 // // - If it is odd, move on to the next variable
 // - While traversing swap the elements inside
 
+
+// CODE :
+
+const sortArrayByParity = (arr) => {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex < rightIndex) {
+        while (arr[leftIndex] % 2 === 0) {
+            leftIndex += 1;
+        }
+        while (arr[rightIndex] % 2 !== 0) {
+            rightIndex -= 1;
+        }
+        if (leftIndex < rightIndex) {
+            const temp = arr[leftIndex];
+            arr[leftIndex] = arr[rightIndex];
+            arr[rightIndex] = temp;
+            leftIndex += 1;
+            rightIndex -= 1;
+        }
+    } 
+    return arr;
+}
