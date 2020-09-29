430b1ab4293bc56e09ef9c8991fa02231b0b7b12
George McCadden III <thethirdone.g@gmail.com>
2020-09-28 18:52:35 -0700

    Giving a clear example

--- sortArrayByParity/index.js
+++ sortArrayByParity/index.js
@@ -1,3 +1,10 @@
 // Problem Domain :
 
 // Given an array 'arr' of non-negative numbers, return an array consisting of all of the even elements of 'arr', forllowed by all of the odd.
+
+
+// Example :
+
+// Input - [3, 1, 2, 4, 11, 26, 9]
+// Output - [3, 1, 11, 9, 2, 4, 26] or any other variation that meets the requirements
+
