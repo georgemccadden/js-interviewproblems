0639e8a5810c73af58e3e1176887b738a0723069
George McCadden III <thethirdone.g@gmail.com>
2020-09-28 18:56:11 -0700

    Writing a clear algorithm

--- sortArrayByParity/index.js
+++ sortArrayByParity/index.js
@@ -8,3 +8,13 @@
 // Input - [3, 1, 2, 4, 11, 26, 9]
 // Output - [3, 1, 11, 9, 2, 4, 26] or any other variation that meets the requirements
 
+
+// Algorithm :
+
+// - Keep track of both directions you'll be moving inside the array in separate variables
+// - Iterate from left to right and check if the element is even
+// // - If it is even, move on to the next variable
+// - Iterate from right to left and check if the element is odd
+// // - If it is odd, move on to the next variable
+// - While traversing swap the elements inside
+
