1739f12c7526b298179e2681c1ff50f6029ec8b9
George McCadden III <thethirdone.g@gmail.com>
2020-10-06 23:51:40 -0700

    Giving a clear example and explanation

--- findUniqChar/index.js
+++ findUniqChar/index.js
@@ -3,3 +3,13 @@
 // Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
 
 
+// Example :
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+
+// Explanation - With each character in the string a check must be performed to see when or if that character appears in the remaining indices of the string. As you search, the first element which does not have any repeated characters is the one which index must be returned.
+
